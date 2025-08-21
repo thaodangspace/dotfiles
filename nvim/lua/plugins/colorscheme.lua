72d470f8ab55adf262b5b3db51a4bb9ba24a7d94
dt <dangthao.stuk14@gmail.com>
2025-08-21 18:37:00 +0700

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,48 @@
+-- lua/plugins/colorscheme.lua
 return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = true,
     priority = 1000,
-    opts = function()
-      return {
-        transparent_background = true,
-      }
+    -- luôn lấy bản mới để tránh lệch API
+    version = false,
+    opts = {
+      transparent_background = true,
+      flavour = "mocha", -- tuỳ chọn: latte, frappe, macchiato, mocha
+      integrations = {
+        bufferline = true, -- rất quan trọng: bật integration cho bufferline
+        treesitter = true,
+        cmp = true,
+        gitsigns = true,
+        telescope = { enabled = true },
+        which_key = true,
+        mason = true,
+        noice = true,
+        notify = true,
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+    end,
+  },
+
+  -- 2) Nói với LazyVim dùng catppuccin làm colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  -- 3) (Khuyến nghị) Để bufferline dùng highlight từ Catppuccin
+  {
+    "akinsho/bufferline.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local catppuccin_hl = require("catppuccin.groups.integrations.bufferline").get()
+      opts = opts or {}
+      opts.highlights = catppuccin_hl
+      return opts
     end,
   },
 }
