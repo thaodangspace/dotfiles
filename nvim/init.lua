f2ed440b644bd6ccc1ef4ae0340450742f179edc
dt <dangthao.stuk14@gmail.com>
2025-08-29 12:20:15 +0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.opt.termguicolors = true
-
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+else
+  -- ordinary Neovim
+  vim.opt.termguicolors = true
+  vim.o.autoread = true
+  vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = { "*" },
+  })
+  require("config.lazy")
+end
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,13 +4,12 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    -- luôn lấy bản mới để tránh lệch API
     version = false,
     opts = {
       transparent_background = true,
-      flavour = "mocha", -- tuỳ chọn: latte, frappe, macchiato, mocha
+      flavour = "mocha",
       integrations = {
-        bufferline = true, -- rất quan trọng: bật integration cho bufferline
+        bufferline = true,
         treesitter = true,
         cmp = true,
         gitsigns = true,
@@ -26,7 +25,6 @@ return {
     end,
   },
 
-  -- 2) Nói với LazyVim dùng catppuccin làm colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
@@ -34,7 +32,6 @@ return {
     },
   },
 
-  -- 3) (Khuyến nghị) Để bufferline dùng highlight từ Catppuccin
   {
     "akinsho/bufferline.nvim",
     optional = true,
