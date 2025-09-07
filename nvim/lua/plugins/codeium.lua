987ec4940eeb03868f0cc8a1bfc505faf6dd73e0
dt <dangthao.stuk14@gmail.com>
2025-09-08 00:01:50 +0700

    update

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -4,26 +4,11 @@ return {
     "Exafunction/codeium.vim",
     event = "BufEnter",
     config = function()
-      -- map mặc định:
       --   <Tab>       : accept suggestion
       --   <C-]>       : cycle next
       --   <C-[>       : cycle previous
       --   <C-x>       : clear
       --
-      -- bạn có thể custom keymap như sau:
-      vim.g.codeium_disable_bindings = 1
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true })
-      vim.keymap.set("i", "<C-;>", function()
-        return vim.fn
-      end, { expr = true })
-      vim.keymap.set("i", "<C-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true })
-      vim.keymap.set("i", "<C-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true })
     end,
   },
 }
