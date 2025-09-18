ad487eb6ace54a6383f0a6af0e5257e9da15ca58
dt <dangthao.stuk14@gmail.com>
2025-09-18 22:04:24 +0700

    update

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -5,18 +5,18 @@
 
 # Define an array of random background colors for focused workspace
 COLORS=(
-  "0xffe74c3c"  # Red
-  "0xff3498db"  # Blue
-  "0xff2ecc71"  # Green
-  "0xfff39c12"  # Orange
-  "0xff9b59b6"  # Purple
-  "0xff1abc9c"  # Teal
-  "0xffe67e22"  # Dark orange
-  "0xff34495e"  # Dark blue-gray
-  "0xfff1c40f"  # Yellow
-  "0xffe91e63"  # Pink
-  "0xff95a5a6"  # Gray
-  "0xfffd79a8"  # Light pink
+  "0xffe74c3c" # Red
+  "0xff3498db" # Blue
+  "0xff2ecc71" # Green
+  "0xfff39c12" # Orange
+  "0xff9b59b6" # Purple
+  "0xff1abc9c" # Teal
+  "0xffe67e22" # Dark orange
+  "0xff34495e" # Dark blue-gray
+  "0xfff1c40f" # Yellow
+  "0xffe91e63" # Pink
+  "0xff95a5a6" # Gray
+  "0xfffd79a8" # Light pink
 )
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
